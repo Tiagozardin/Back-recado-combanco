2c8c65a31553c1cd58e2fbadd281de646a05a7ed
Tiago Zardin <tiagozardin@hotmail.com>
2021-07-17 09:45:45 -0300

    get

--- src/features/login/routes/loginRoutes.ts
+++ src/features/login/routes/loginRoutes.ts
@@ -11,7 +11,8 @@ export default class LoginRoutes {
     const controller = new LoginController();
 
     routes.post("/createLogin", [LoginCamposObrigatorios, LoginJaExiste], controller.store);
-    routes.post('/login',[validateNotExistUser, validatePassword], controller.login);
+    routes.post('/login',[validateNotExistUser, LoginCamposObrigatorios, validatePassword], controller.login);
+    routes.get("/login", controller.index);
     return routes;
   }
 }
